bfdfc681b0f05b43089e90b1463a350adb0e9d2f
wptomo <wptomo1106@gmail.com>
2020-05-20 15:25:18 +0800

    Update Editor.php

--- src/Editor.php
+++ src/Editor.php
@@ -7,9 +7,11 @@ use Encore\Admin\Form\Field;
 class Editor extends Field
 {
     protected $view = 'laravel-admin-wang-editor2::editor';
+    
     protected static $css = [
         'vendor/laravel-admin-ext/wang-editor2/wangEditor-2.1.23/dist/css/wangEditor.min.css',
     ];
+    
     protected static $js = [
         'vendor/laravel-admin-ext/wang-editor2/wangEditor-2.1.23/dist/js/wangEditor.min.js',
     ];
@@ -20,16 +22,23 @@ class Editor extends Field
         $fileName = config('admin.extensions.wang-editor2.config.uploadImgFileName');
         $api = config('admin.extensions.wang-editor2.config.uploadImgUrl');
         $menus = json_encode(config('admin.extensions.wang-editor2.config.menus'));
+        $false = false;
         $this->script = <<<EOT
 
-var editor = new wangEditor('{$this->id}');
-    editor.config.menus = {$menus};
-    editor.config.uploadImgFileName = '{$fileName}';
-    editor.config.uploadImgUrl = '{$api}';
+wangEditor.config.printLog = '{$false}'
+
+var editor = new wangEditor('{$this->id}')
+    editor.config.menus = {$menus}
+    editor.config.uploadImgFileName = '{$fileName}'
+    editor.config.uploadImgUrl = '{$api}'
+    editor.config.pasteFilter = '{$false}'
     editor.config.uploadParams = {
-    _token: '$token'
-};
-    editor.create();
+        '_token': '$token',
+    }
+    editor.config.uploadHeaders = {
+        'Accept': 'application/json'
+    }
+    editor.create()
 
 EOT;
         return parent::render();
